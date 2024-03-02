499508a828cbb31994c86daa3bd523a09bf61c9e
Snowball <bryce518449@gmail.com>
2024-03-02 14:43:19 -0600

    wow server.js

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -3,9 +3,9 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
-		.setDescription('Provides information about the server.'),
+		.setDescription('Provides information about the server.🤓🤓'),
 	async execute(interaction) {
 		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		await interaction.reply({content: `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`, ephemeral: true });
 	},
 };
